be526cf1596ab101f8eded4e96aa0cab9ff7142e
tuhage <tuhage@gmail.com>
2026-01-30 17:03:32 +0300

    Initial commit of the Cellar project, including the project structure, main application file, and initial user interface. Added assets for app icon and accent color, along with necessary configuration files for Xcode workspace and schemes.

---
+++ Cellar/CellarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CellarApp.swift
+//  Cellar
+//
+//  Created by Hakan Gençtürk on 30.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct CellarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
